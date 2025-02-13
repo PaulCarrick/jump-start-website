d2e2ae3778303cb82070b7746fe88063134c719d
Paul Carrick <paulandvirginiacarrick@gmail.com>
2025-02-13 11:26:30 -0800

    Fix package name.

--- startup/debconf/configure.sh
+++ startup/debconf/configure.sh
@@ -6,7 +6,7 @@ set -e
 
 sudo apt update
 sudo apt install -y pip
-sudo pip install validations --break-system-packages
+sudo pip install validators --break-system-packages
 sudo pip install psycopg2-binary --break-system-packages
 sudo pip install python-dotenv --break-system-packages
 
