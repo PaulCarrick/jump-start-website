6a83962895458777b5a0af5011738eaa40a6c716
Paul Carrick <paulandvirginiacarrick@gmail.com>
2025-02-15 05:52:55 -0800

    Update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -12,15 +12,15 @@ validators=`echo ${pip_list} | grep "validators"`
 psychopg=`echo ${pip_list} | grep "psycopg2-binary"`
 dotenv=`echo ${pip_list} | grep "python-dotenv"`
 
-if [ -z "${validators}"]; then
+if [ -z "${validators}" ]; then
   sudo pip install validators --break-system-packages
 fi
 
-if [ -z "${psychopg}"]; then
+if [ -z "${psychopg}" ]; then
   sudo pip install psycopg2-binary --break-system-packages
 fi
 
-if [ -z "${dotenv}"]; then
+if [ -z "${dotenv}" ]; then
   sudo pip install python-dotenv --break-system-packages
 fi
 
