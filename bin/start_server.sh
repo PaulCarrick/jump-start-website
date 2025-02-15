6da6b02c31be0109363a6b358525d041ff7f4060
Paul Carrick <paulandvirginiacarrick@gmail.com>
2025-02-15 06:33:31 -0800

    Update start_server.sh

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
 
 # Setup the environment
-
 if [ -z "${SERVER_HOST}" ]; then
     parent_directory="$(cd "$(dirname "$0")" && pwd)/.."
 
     if [ -f "${parent_directory}/.env" ]; then
-      set -a
+        set -a
         . "${parent_directory}/.env"
-
         if [ $? -ne 0 ]; then
-          echo "*** Cannot load environment file ${parent_directory}/.env ***" >&2
-          exit 2
+            echo "*** Cannot load environment file ${parent_directory}/.env ***" >&2
+            exit 2
         fi
-      set +a
+        set +a
     else
-      echo "*** Cannot find environment file ${parent_directory}/.env ***" >&2
-      exit 1
+        echo "*** Cannot find environment file ${parent_directory}/.env ***" >&2
+        exit 1
     fi
 fi
 
 PROCESS_NAME="puma"
 
+# Ensure RAILS_DIRECTORY is set
 if [ -z "${RAILS_DIRECTORY}" ]; then
     export RAILS_DIRECTORY="$(cd "$(dirname "$0")" && pwd)/.."
 fi
@@ -36,36 +35,36 @@ if [ -n "$PUMA_PID" ]; then
     kill "$PUMA_PID"
     echo "$PROCESS_NAME process terminated."
 
-    # Add a delay to ensure the process is fully terminated
+    # Ensure the process is fully terminated
     DELAY_SECONDS=3
     echo "Waiting for $DELAY_SECONDS seconds before starting the server..."
     sleep "$DELAY_SECONDS"
 else
-    echo "No $PROCESS_NAME process found running on port $PORT."
+    echo "No $PROCESS_NAME process found running."
 fi
 
-cd "${RAILS_DIRECTORY}"
-
-if [ $? -ne 0 ]; then
-    echo "Can't change to ${RAILS_DIRECTORY} directory." >&2
-    exit 3
+# Ensure SERVER_PORT is set
+if [ -z "$SERVER_PORT" ]; then
+    echo "SERVER_PORT is not set. Using default port 3000."
+    SERVER_PORT=3000
 fi
+
+cd "${RAILS_DIRECTORY}" || { echo "Can't change to ${RAILS_DIRECTORY} directory." >&2; exit 3; }
 
 # Start the Puma server
 echo "Starting the server..."
 
-if [ "${NOHUP}" == "true" ]; then
-  nohup "${RAILS_DIRECTORY}/bin/bundle" exec rails s > log/server.log 2>&1 &
-  sleep 3
+if [ "$NOHUP" = "true" ]; then
+    nohup "${RAILS_DIRECTORY}/bin/bundle" exec rails s -b 0.0.0.0 -p "$SERVER_PORT" > log/server.log 2>&1 &
+    sleep 3
 else
-  echo "Running... ${RAILS_DIRECTORY}/bin/bundle" exec rails s -b 0.0.0.0 -p ${SERVER_PORT}" >%2
+    echo "Running: ${RAILS_DIRECTORY}/bin/bundle exec rails s -b 0.0.0.0 -p $SERVER_PORT"
+    "${RAILS_DIRECTORY}/bin/bundle" exec rails s -b 0.0.0.0 -p "$SERVER_PORT"
 
-  "${RAILS_DIRECTORY}/bin/bundle" exec rails s -b 0.0.0.0 -p "${SERVER_PORT}"
-
-  if [ $? -ne 0 ]; then
-    echo "Can't run ${RAILS_DIRECTORY}/bin/bundle exec rails s -b 0.0.0.0 -p ${SERVER_PORT}."
-    exit 4
-  fi
+    if [ $? -ne 0 ]; then
+        echo "Can't run ${RAILS_DIRECTORY}/bin/bundle exec rails s -b 0.0.0.0 -p $SERVER_PORT." >&2
+        exit 4
+    fi
 fi
 
 exit 0
